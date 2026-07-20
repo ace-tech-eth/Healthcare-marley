418a29712754ab887570a8033898d538f7f87be6
philliyg <36069535+philliyg@users.noreply.github.com>
2026-07-21 00:21:14 +0300

    refactor: store signatures on practitioners

--- healthcare/patches/v16_0/setup_prescription_signature.py
+++ healthcare/patches/v16_0/setup_prescription_signature.py
@@ -2,11 +2,11 @@ import frappe
 
 
 def execute():
-	for doctype in ["practitioner_signature", "prescription_signature", "medication_prescription"]:
+	for doctype in ["healthcare_practitioner", "prescription_signature", "medication_prescription"]:
 		try:
 			frappe.reload_doc("healthcare", "doctype", doctype)
 		except Exception:
 			frappe.log_error(frappe.get_traceback(), f"Could not reload {doctype}")
 
-	for doctype in ["Practitioner Signature", "Prescription Signature", "Medication Prescription"]:
+	for doctype in ["Healthcare Practitioner", "Prescription Signature", "Medication Prescription"]:
 		frappe.clear_cache(doctype=doctype)
